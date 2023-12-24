7dbcd1c52d0e99e4e55806a2750148811c075728
youssef <ketajyoussef@gmail.com>
2023-12-24 14:38:35 +0000

    project started 8

---
+++ Pharmacist/dashboard.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+if (!($_SESSION['role'] == 'Pharmacist')) {
+    header('location: ../error404.php');
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="en" class="h-full">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DASHBOARD</title>
+    <link href="../Assets/css/tailwind.css" rel="stylesheet">
+</head>
+<body class="bg-gray-50 dark:bg-slate-900">
+<?php require '../Assets/components/header.php'?>
+<?php require '../Assets/components/pharmasistmenu.php'?>
+<div class="w-full pt-10 px-4 sm:px-6 md:px-8 lg:ps-72">
+
+</div>
+<script src="../node_modules/preline/dist/preline.js"></script>
+</body>
+</html>
