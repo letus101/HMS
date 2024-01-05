f897a510d4561d7b91c61dba28738412824be679
youssef <ketajyoussef@gmail.com>
2024-01-05 19:10:41 +0000

    project  design

--- Assets/components/labtechmenu.php
+++ Assets/components/labtechmenu.php
@@ -1,3 +1,34 @@
+<div class="sticky top-0 inset-x-0 z-20 bg-white border-y px-4 sm:px-6 md:px-8 lg:hidden dark:bg-gray-800 dark:border-gray-700">
+    <div class="flex items-center py-4">
+        <button type="button" class="text-gray-500 hover:text-gray-600" data-hs-overlay="#application-sidebar" aria-controls="application-sidebar" aria-label="Toggle navigation">
+            <span class="sr-only">Toggle Navigation</span>
+            <svg class="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" x2="21" y1="6" y2="6"/><line x1="3" x2="21" y1="12" y2="12"/><line x1="3" x2="21" y1="18" y2="18"/></svg>
+        </button>
+        <div class="ml-4">
+            <p class="text-sm text-gray-500 flex flex-wrap">
+                <?php
+                $path = $_SERVER['PHP_SELF'];
+                $path_parts = explode('/', $path);
+                // Remove 'HMS22' from the path segments
+                array_shift($path_parts);
+                array_shift($path_parts);
+                $last_index = count($path_parts) - 1;
+                foreach ($path_parts as $index => $part) {
+                    $part = pathinfo($part, PATHINFO_FILENAME);
+                    // If it's the last part, make it bold
+                    if ($index == $last_index) {
+                        echo "<strong>$part</strong>";
+                    } else {
+                        echo $part;
+                        // Add '>' as a separator
+                        echo ' > ';
+                    }
+                }
+                ?>
+            </p>
+        </div>
+    </div>
+</div>
 <div id="application-sidebar" class="hs-overlay hs-overlay-open:translate-x-0 -translate-x-full transition-all duration-300 transform hidden fixed top-0 start-0 bottom-0 z-[60] w-64 bg-white border-e border-gray-200 pt-7 pb-10 overflow-y-auto lg:block lg:translate-x-0 lg:end-auto lg:bottom-0 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-slate-700 dark:[&::-webkit-scrollbar-thumb]:bg-slate-500 dark:bg-gray-800 dark:border-gray-700">
     <div class="px-6">
         <a class="flex-none text-xl font-semibold dark:text-white dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="dashboard.php" aria-label="Brand">HOSPITALINK</a>
